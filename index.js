e04e95efae66b6590f004db64065e14af555aa3e
William James <52788612+william-james-pj@users.noreply.github.com>
2021-02-08 11:05:37 -0300

    Init project

---
+++ index.js
@@ -0,0 +1,13 @@
+const bodyParser = require('body-parser')
+const express = require("express")
+const app = express()
+const router = require("./routes/routes")
+ 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use("/",router);
+
+app.listen(8080,() => {
+    console.log("Servidor rodando")
+});
